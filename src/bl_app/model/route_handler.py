26bb5cbc0d130ae7a87b16a27000d9f79e55fe48
Leon Nascimento <l.nasc@live.com>
2018-10-20 21:06:39 -0300

    Incluído API_ROUTE como constante e determinado retornos a partir dela

--- src/bl_app/model/route_handler.py
+++ src/bl_app/model/route_handler.py
@@ -2,25 +2,28 @@
 from model.postman import Postman
 from model.getman import Getman
 
+API_ROUTE = 'api/blog/posts'
+
 class RouteHandler:
   # classe que define as funções de rota
 
   def __init__(self):
-    #Inicia uma instância de rotas
+    #Inicia uma instância dos gerenciadores de rota
     self.postman = Postman()
     self.getman = Getman()
 
   def make_new_post(self, data):
-    #data é o valor de um form em request do Flask
-    #write_post formata os dados de forma 'amigável' ao BD
-    data =self.postman.write_post(data) 
+    #data é o valor de um form em um request para o Flask
+    #write formata os dados de forma 'amigável' ao BD
+    data = self.postman.write(data) 
     post_name = self.postman.post(data)
 
     return '%s/%s' % (API_ROUTE, post_name)
 
   def edit_post(self, post_name, data):
     post_id = self.getman.get_uniqid(post_name)
-    updated_post = self.postman.write_post(data)
+
+    updated_post = self.postman.write(data)
     self.postman.rewrite(post_id, updated_post)
 
     return '%s/%s' % (API_ROUTE, post_name)
@@ -34,6 +37,9 @@ class RouteHandler:
   def get_post_by_id(self,post_name):
     post_id = self.getman.get_uniqid(post_name)
     return self.getman.get_post(post_id)
+    
+  def get_posts_by_date(self,month,year):
+    return self.getman.get_monthly_posts(month,year)
 
   def get_recent_posts(self, amount=20):
     #Se não definido como parametro, o padrão são 20 posts
