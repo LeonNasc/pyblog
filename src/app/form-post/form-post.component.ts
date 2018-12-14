bae1de9a42abb95571c068844bcc7d29b447a37a
leonnasc <l.nasc@live.com>
2018-12-13 23:47:21 -0200

    Acertado formatação do JSON no back-end

--- src/app/form-post/form-post.component.ts
+++ src/app/form-post/form-post.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-form-post',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-post.component.css']
 })
 export class FormPostComponent implements OnInit {
+	
+  // Todos os inputs do formulário são two-way binded a estas variaveis
+  autor = '';
+  titulo = '';
+  conteudo = '';
+  tags= '';
+	
+  test= '';
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   ngOnInit() {
   }
 
+  ngHttpTest(){
+	let url = 'http://172.17.0.2:5000/api/v1/blog/posts/recents';
+	let test = this.http.get(url);
+	test.subscribe((data) =>this.test = data);
+  }
+
+  //Envia um post para o servidor usando o Http service.
+  sendPostForm(){
+	//TODO
+
+  }
+
+  
 }
