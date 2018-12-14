8ca22929aaf377e3e78415590b810bb71cc7047a
leonnasc <l.nasc@live.com>
2018-12-14 00:55:12 -0200

    Problemas com CORS ao usar o Angular para abstrair as requisições ao servidor

--- src/app/form-post/form-post.component.ts
+++ src/app/form-post/form-post.component.ts
@@ -1,37 +1,50 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-form-post',
   templateUrl: './form-post.component.html',
   styleUrls: ['./form-post.component.css']
 })
-export class FormPostComponent implements OnInit {
+export class FormPostComponent {
 	
   // Todos os inputs do formulário são two-way binded a estas variaveis
   autor = '';
   titulo = '';
   conteudo = '';
   tags= '';
-	
-  test= '';
+  private base_url = 'http://172.17.0.2:5000/api/v1/blog/';
 
   constructor(private http:HttpClient) { }
 
-  ngOnInit() {
-  }
-
   ngHttpTest(){
-	let url = 'http://172.17.0.2:5000/api/v1/blog/posts/recents';
-	let test = this.http.get(url);
-	test.subscribe((data) =>this.test = data['posts']);
+	let test = this.http.get(this.base_url+"posts/recents");
+	test.subscribe((data) =>this.autor = data['posts']);
   }
 
   //Envia um post para o servidor usando o Http service.
   sendPostForm(){
-	//TODO
 
+  const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':  'application/json',
+  })
+  };
+
+  let schema = {"autor":this.autor,
+  "titulo":this.titulo,
+  "conteudo":this.conteudo,
+  "tags":this.tags}
+
+	if(this.isValidSchema(schema)){
+		this.http.post(this.base_url+"posts/new",schema,httpOptions).subscribe((data) => console.log(data))
+	}
   }
-
-  
+  isValidSchema(schema){
+	for(var key in schema){
+		if(schema[key]==false)
+			return false
+	}
+	return true;
+  }  
 }
