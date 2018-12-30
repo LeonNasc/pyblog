2c8060e55c0bd6bb87d357f017fe15ccb6ffcc45
leonnasc <l.nasc@live.com>
2018-12-29 22:51:05 -0200

    Imagens centralizadas

--- src/app/post-list/post-list.component.ts
+++ src/app/post-list/post-list.component.ts
@@ -29,6 +29,10 @@ export class PostListComponent extends PostableComponent implements OnInit {
 			}
 		  }
 	  )
+
   }
+	ngAfterViewChecked() {
+	  setTimeout(document.querySelectorAll(".card img").forEach((el) => el.parentElement.style.textAlign = "center"),1500);
+	}
 
 }
